00568c5fde65c2ff5783dc45922917de50b3d05b
dxicode <dxicode@gmail.com>
2025-01-06 22:03:27 -0600

    Actualización iniciar-sesion.blade.php

--- hecho-en-casa-GH/resources/views/iniciar-sesion.blade.php
+++ hecho-en-casa-GH/resources/views/iniciar-sesion.blade.php
@@ -2,33 +2,51 @@
 <title>Iniciar sesión</title>
 <x-menu />    
 
-<div class = "contenedor">   
-    
-    <form action="{{route('login.post')}}" method="POST" id="inicioSesion">
-        @csrf
+<div class="flexi">
+<div class = "contenedor">         
+    <form action="procesar.php" method="POST" id = "inicioSesion">
         <h2>Iniciar sesión</h2>
         <label for="email">Correo: </label>
-        <input type="email" id = "email" name = "correo_electronico" required> 
+        <input type="email" id = "email" name = "email" required> 
         <div class="mensaje">
         <p id="errorEmail" class="error"></p>
         <p id="bienEmail" class="bien"></p>
         </div>
 
         <br>
-        <label for="password">Contraseña: </label>
-        <input type="password" id = "pass" name ="contraseña"> 
+        <div class="alineando">
+            <label for="password">Contraseña: </label>
+            <div class="campo-contrasena">
+                <input type="password" id="password" name="password" required>
+                <i class="fi fi-rs-crossed-eye visibility" onclick="visibility('password', this)"></i>
+            </div>
+        </div>
         <div class="mensaje">
         <p id="errorPass" class="error"></p>
         <p id="bienPass" class="bien"></p>
         </div>
-        <div>
-            <button class="botoncito" type="submit" name="action" value="recuperar" id="olvidadizo">Olvide mi contraseña</button>
-            <button class="botoncito" type="submit" name="action" value="register">Registrarme</button>
-            <button class="botoncito" type="submit" name="action" value="login" onclick="validateForm()">Continuar</button>
-        </div>
-    </form>
+
+        <button id="olvidadizo" type="submit" name="action" value="register">Olvidé mi contraseña</button>
+        <br><br>
+
+    <div>
+        <button class="botoncito" type="submit" name="action" value="register">Registrarme</button>
+        <button class="botoncito" type="submit" name="action" value="login" onclick="validateForm()">Continuar</button>
+    </div>
+</form>
 </div>
+
+<div class="fondo-emergente" id="fondoEmergente">
+    <div class="emergente">    
+        <p class="mensajeEmergente">Se ha enviado un código de seguridad a tu correo.</p>
+        <p class="mensajeEmergente"></p>
+        <button id="aceptar" class="aceptando">✔</button>
+    </div>
 </div>
+
+</div>
+
+
 <x-pie/>
 
 <script src="{{ asset('js/iniciando.js') }}"></script>
